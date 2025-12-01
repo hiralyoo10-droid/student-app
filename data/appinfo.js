e7567ceb7a465fa1a0d974f75863aa7dd50aafa0
hiralyoo10-droid <hiralyoo10@gmail>
2025-12-01 17:22:52 +0700

    first commit

---
+++ data/appinfo.js
@@ -0,0 +1,10 @@
+
+
+const appinfo = {
+    title: "StudentCares",
+    subtitle: "Empowering Student for a Brighter Futere",
+    schoolName: "NVC",
+    version: " 1.0.0 "
+};
+
+export default appinfo;
